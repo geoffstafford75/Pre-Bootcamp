2b0c3cfe4b77218aaaac8f962770c074b14d3fe6
Geoff Stafford <geoffreystafford@gmail.com>
2021-10-20 21:24:33 -0700

    for_loop

---
+++ Geoff_Stafford_JS_Tinkering.js
@@ -0,0 +1,11 @@
+var childHeight = 58;
+
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+    if (childHeight>52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry Kiddo. Maybe next year");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
